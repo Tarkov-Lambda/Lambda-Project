d90c0291d7a7a8557996c82e58218943b7d3215f
bmpq <73074311+bmpq@users.noreply.github.com>
2026-03-06 22:27:54 +0300

    bomba interaction zone

---
+++ arena.bep/Patches/Tarkov/Patch_InteractionContextHelper_GetAvailableActions.cs
@@ -0,0 +1,41 @@
+﻿using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using ifp.arena.shared;
+using System;
+
+using InteractionContextHelper = GetActionsClass;
+using IInteractive = GInterface177;
+
+namespace ifp.arena.bep.Patches.Tarkov
+{
+    internal class Patch_InteractionContextHelper_GetAvailableActions : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(InteractionContextHelper), nameof(InteractionContextHelper.GetAvailableActions), [typeof(GamePlayerOwner), typeof(IInteractive)]);
+        }
+
+        [PatchPrefix]
+        private static bool PatchPrefix(ref ActionsReturnClass __result, GamePlayerOwner owner, IInteractive interactive)
+        {
+            BombPlantZone plantZone = interactive as BombPlantZone;
+            if (plantZone == null)
+                return true;
+
+            ActionsReturnClass actionsReturnClass = new ActionsReturnClass();
+            actionsReturnClass.Actions.Add(new ActionsTypesClass
+            {
+                Name = "Plant",
+                Action = new Action(Plant)
+            });
+
+            return false;
+        }
+
+        static void Plant()
+        {
+        }
+    }
+}
--- arena.shared/BombPlantZone.cs
+++ arena.shared/BombPlantZone.cs
@@ -1,11 +1,20 @@
-﻿using EFT;
+﻿#if EFT_RUNTIME
+using EFT;
+
+using EFT.Interactive;
+#endif
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace ifp.arena.shared
 {
-    public class BombPlantZone : MonoBehaviour
+    public class BombPlantZone :
+#if EFT_RUNTIME
+        InteractableObject
+#else
+        MonoBehaviour
+#endif
     {
 
     }
