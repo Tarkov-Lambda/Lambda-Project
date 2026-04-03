accfc9745141ef9079b104a14293a756c8a82bdf
bmpq <73074311+bmpq@users.noreply.github.com>
2026-04-03 07:52:42 +0300

    death info

--- arena.ui/ArenaMatchUI.cs
+++ arena.ui/ArenaMatchUI.cs
@@ -12,6 +12,7 @@ namespace arena.ui
         [field: SerializeField] public Scoreboard Scoreboard { get; private set; }
         [field: SerializeField] public KillFeed KillFeed { get; private set; }
         [field: SerializeField] public PopupMatchEnd PopupMatchEnd { get; private set; }
+        [field: SerializeField] public DeathInfo DeathInfo { get; private set; }
 
         public void ToggleScoreboard(bool show)
         {
---
+++ arena.ui/DeathInfo/DeathInfo.cs
@@ -0,0 +1,40 @@
+using ifp.arena.shared.Models;
+using TMPro;
+using UnityEngine;
+
+namespace arena.ui
+{
+    public class DeathInfo : MonoBehaviour
+    {
+        [SerializeField] CanvasGroup frame;
+
+        [SerializeField] private TMP_Text textKillerName;
+        [SerializeField] private FactionColors factionColors;
+
+        float t;
+
+        void Awake()
+        {
+            frame.alpha = 1f;
+        }
+
+        public void Pop(PlayerStats killer)
+        {
+            textKillerName.name = killer.Name;
+            textKillerName.color = factionColors.Get(killer.Faction);
+
+            frame.alpha = 1f;
+            t = 0f;
+        }
+
+        void Update()
+        {
+            if (t < 1f)
+            {
+                t += Time.deltaTime * 3f;
+                t = Mathf.Clamp01(t);
+                frame.alpha = EasingFunctions.InCirc(1f - t);
+            }
+        }
+    }
+}
