6cf18904a6397ad5d308bb55d69b03e51230b770
bmpq <73074311+bmpq@users.noreply.github.com>
2026-04-04 10:30:13 +0300

    upscaling support fr fr

--- arena.bep/Core/UI/ScreenGrabber.cs
+++ arena.bep/Core/UI/ScreenGrabber.cs
@@ -6,6 +6,8 @@ namespace ifp.arena.bep.Core.UI
     internal class EFTScreenGrabber : MonoBehaviour
     {
         private Camera _camera;
+        private SSAA _ssaa;
+
         private CommandBuffer _commandBuffer;
         private RenderTexture _screenCapture;
 
@@ -18,9 +20,14 @@ namespace ifp.arena.bep.Core.UI
                 return;
             }
 
+            _ssaa = _camera.GetComponent<SSAA>();
+
             _commandBuffer = new CommandBuffer { name = "UI_GrabScreen" };
 
-            _camera.AddCommandBuffer(CameraEvent.AfterEverything, _commandBuffer);
+            _camera.AddCommandBuffer(CameraEvent.AfterImageEffects, _commandBuffer);
+
+            if (_ssaa != null)
+                _ssaa.GetComponent<SSAAImpl>().RenderTexturesAreChanged += RebuildResources;
 
             RebuildResources();
         }
@@ -37,9 +44,23 @@ namespace ifp.arena.bep.Core.UI
                 _screenCapture = null;
             }
 
-            _screenCapture = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBHalf);
+            int w = Screen.width;
+            int h = Screen.height;
+
+            _screenCapture = new RenderTexture(w, h, 0, RenderTextureFormat.ARGBHalf);
             _screenCapture.name = "GlobalUI_ScreenGrab";
             Shader.SetGlobalTexture("_GlobalScreenGrab", _screenCapture);
+
+            if (_ssaa != null)
+            {
+                RenderTexture src, dst;
+                var prop = _ssaa.GetComponent<SSAAPropagator>();
+                if (prop != null && prop.GetSourceDestination(out src, out dst) && src != null)
+                {
+                    _commandBuffer.Blit(src, _screenCapture, new Vector2(1, -1f), new Vector2(0, 1));
+                    return;
+                }
+            }
 
             _commandBuffer.Blit(BuiltinRenderTextureType.CameraTarget, _screenCapture);
         }
@@ -47,8 +68,12 @@ namespace ifp.arena.bep.Core.UI
         void OnDisable()
         {
             if (_camera != null && _commandBuffer != null)
+                _camera.RemoveCommandBuffer(CameraEvent.AfterImageEffects, _commandBuffer);
+
+            if (_ssaa != null)
             {
-                _camera.RemoveCommandBuffer(CameraEvent.AfterEverything, _commandBuffer);
+                var impl = _ssaa.GetComponent<SSAAImpl>();
+                if (impl != null) impl.RenderTexturesAreChanged -= RebuildResources;
             }
 
             if (_commandBuffer != null) _commandBuffer.Release();
