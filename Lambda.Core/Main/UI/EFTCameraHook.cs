a59ed63da98fb6ee7ce3f8aa1503aec9d7eafdac
bmpq <73074311+bmpq@users.noreply.github.com>
2026-05-14 07:10:45 +0300

    fix camera change logic

--- Lambda.Core/Main/UI/EFTCameraHook.cs
+++ Lambda.Core/Main/UI/EFTCameraHook.cs
@@ -1,35 +1,60 @@
-﻿using System;
+﻿using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
 using UnityEngine;
 
 namespace Lambda.Core.Main.UI;
 
 internal class EFTCameraHook : IDisposable
 {
+    readonly ModulePatch _patch;
+
     internal EFTCameraHook()
     {
-        CameraManager.Instance.OnCameraChanged += OnCameraChanged;
+        Patch_CameraManager_SetCamera.OnPostfix += OnCameraChanged;
+        if (CameraManager.Exist)
+            OnCameraChanged(CameraManager.Instance);
 
-        if (CameraManager.Instance.Camera != null)
-        {
-            OnCameraChanged();
-        }
-    }
-
-    void OnCameraChanged()
-    {
-        CameraManager.Instance.Camera.gameObject.GetOrAddComponent<EFTScreenGrabber>();
+        _patch = new Patch_CameraManager_SetCamera();
+        _patch.Enable();
     }
 
     public void Dispose()
     {
-        CameraManager.Instance.OnCameraChanged -= OnCameraChanged;
+        _patch.Disable();
+        Patch_CameraManager_SetCamera.OnPostfix -= OnCameraChanged;
 
-        if (CameraManager.Instance.Camera != null)
+        if (CameraManager.Exist && CameraManager.Instance.Camera != null)
         {
             if (CameraManager.Instance.Camera.TryGetComponent<EFTScreenGrabber>(out var component))
             {
                 Component.Destroy(component);
             }
+        }
+    }
+
+    void OnCameraChanged(CameraManager cameraManager)
+    {
+        if (cameraManager.Camera == null)
+            return;
+
+        cameraManager.Camera.gameObject.GetOrAddComponent<EFTScreenGrabber>();
+    }
+
+    private class Patch_CameraManager_SetCamera : ModulePatch
+    {
+        public static event Action<CameraManager> OnPostfix;
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(CameraManager), nameof(CameraManager.SetCamera));
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(CameraManager __instance, Camera camera)
+        {
+            OnPostfix?.Invoke(__instance);
         }
     }
 }
