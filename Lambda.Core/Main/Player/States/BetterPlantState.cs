b166f9f6db652601a0db0760f75232cb0482f01d
ifpCD <mr.imforpeace.xx@gmail.com>
2026-05-16 16:52:07 -0600

    BetterPlantState cleanup

--- Lambda.Core/Main/Player/States/BetterPlantState.cs
+++ Lambda.Core/Main/Player/States/BetterPlantState.cs
@@ -2,63 +2,46 @@
 using EFT;
 using UnityEngine;
 
-public class BetterPlantStateClass(MovementContext movementContext) : PlantStateClass(movementContext)
+public class BetterPlantStateClass : PlantStateClass
 {
+    private readonly GClass777 _animatorStepper;
+
+    public BetterPlantStateClass(MovementContext movementContext) : base(movementContext)
+    {
+        _animatorStepper = new GClass777();
+        _animatorStepper.Init(movementContext);
+    }
+
     public override void Enter(bool isFromSameState)
     {
         base.Enter(isFromSameState);
 
-        // override the rotation limit set by the base class
-        this.MovementContext.SetRotationLimit(Player.PlayerMovementConstantsClass.FULL_YAW_RANGE, Player.PlayerMovementConstantsClass.STAND_POSE_ROTATION_PITCH_RANGE);
+        MovementContext.SetRotationLimit(Player.PlayerMovementConstantsClass.FULL_YAW_RANGE, Player.PlayerMovementConstantsClass.STAND_POSE_ROTATION_PITCH_RANGE);
 
-        this.Float_1 = this.MovementContext.PoseLevel;
-        this.MovementContext.SetTilt(0f, false);
-        if (!this.PlantMultitool)
-        {
-            this.MovementContext.SetPoseLevel(0f, false);
-        }
-
-        this.MovementContext.SetPatrol(true);
-        this.Float_0 = Time.realtimeSinceStartup;
+        _animatorStepper?.Enter();
     }
 
-    public override void Exit(bool toSameState)
+    public override void SetStep(int step)
     {
-        base.Exit(toSameState);
-
-        if (!this.PlantMultitool)
+        if (Mathf.Abs(step) > 0)
         {
-            this.MovementContext.SetPoseLevel(this.Float_1, false);
+            Vector3 vector = new(MovementContext.TransformForwardVector.z, 0f, -MovementContext.TransformForwardVector.x);
+
+            if (MovementContext.OverlapOrHasNoGround(0.3f, new Vector3?(vector * Mathf.Sign((float)step)), 0.2f, 3f, 0f))
+            {
+                MovementContext.Step = 0;
+                return;
+            }
         }
-        this.MovementContext.SetPatrol(false);
+
+        MovementContext.Step = step;
     }
 
     public override void ManualAnimatorMoveUpdate(float deltaTime)
     {
         base.ManualAnimatorMoveUpdate(deltaTime);
-        if (Time.realtimeSinceStartup - this.Float_0 <= this.PlantTime)
-        {
-            return;
-        }
-        if (this.MovementContext.PlantAction != null)
-        {
-            this.MovementContext.PlantAction(true);
-            this.MovementContext.PlantAction = null;
-        }
-        this.MovementContext.ExitOverridenState();
-    }
 
-    public override void ChangePose(float poseDelta) { }
-
-    public override void SetTilt(float tilt) { }
-
-    public override void Pickup(bool enabled, Action action) { }
-
-    public override void Examine(bool enabled, Action action) { }
-
-    public override void Plant(bool enabled, bool multitool, float plantTime, Action<bool> action)
-    {
-        this.MovementContext.ExitOverridenState();
+        _animatorStepper?.ProcessAnimatorStep(deltaTime, Type);
     }
 
     public override void Cancel()
@@ -66,12 +49,12 @@ public class BetterPlantStateClass(MovementContext movementContext) : PlantState
         // cancelling the freelook doesn't cancel planting
         if (Patch_GamePlayerOwner_TranslateCommand.IsRessetingFreelook) return;
 
-        if (this.MovementContext.PlantAction == null)
+        if (MovementContext.PlantAction == null)
         {
             return;
         }
-        this.MovementContext.PlantAction(false);
-        this.MovementContext.PlantAction = null;
-        this.MovementContext.ExitOverridenState();
+        MovementContext.PlantAction(false);
+        MovementContext.PlantAction = null;
+        MovementContext.ExitOverridenState();
     }
 }
